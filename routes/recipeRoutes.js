cf620b1819cf0bceefcca3db8f78eecf2871818b
malsha7 <malsha74bopage@gmail.com>
2025-08-16 21:30:07 +0530

    Updated Recipe routes

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -4,6 +4,10 @@ const recipeController = require('../controllers/recipeController');
 const authMiddleware = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware'); // for user recipes image uploads
 
+/**
+ * My Recipes (user-created)
+ */
+
 // Create new recipe (my recipes only)
 router.post('/my-recipes',authMiddleware,upload.single('image'),recipeController.createRecipe);
 
@@ -13,9 +17,22 @@ router.put('/my-recipes/:id',authMiddleware,upload.single('image'),recipeControl
 // Delete user recipe (my recipes only)
 router.delete('/my-recipes/:id',authMiddleware,recipeController.deleteRecipe);
 
-// Get all recipes (all recipe list)
+// Get all my recipes
+router.get('/my-recipes', authMiddleware, recipeController.getMyRecipes);
+
+/**
+ * 
+  System Recipes
+ */
+
+// Get all system recipes (Home / Recipe list)
 router.get('/', recipeController.getAllRecipes);
 
 //Get single recipe by ID
 router.get('/:id', recipeController.getRecipeById);
 
+// Suggest recipes (search bar)
+router.get('/suggest/search', recipeController.suggestRecipes);
+
+// Filter recipes - recipe suggesting using tools, mealtime, ingredeints
+router.post('/filter', recipeController.filterRecipes);
